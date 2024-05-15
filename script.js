5e1c793d068113db506753641a202ecd693bb3f6
Eusebiu <eusebiu.leontescu@emanuel.ro>
2024-05-15 23:59:44 +0300

    adaugare fisier .css

--- script.js
+++ script.js
@@ -1,24 +1,17 @@
 function setup(){
-createCanvas (600, 600);
-
+let canvas = createCanvas (600, 600);
+canvas.parent('game-board');
+canvas.center();
 cellSize = width / boardSize;
 board = createBoard();
 player1= createPlayer(4,0,[255,0,0]);
 player2=createPlayer(4,8,[0,0,255]);
 mousePressed();
 movePlayer();
-
-
-
 }
 function draw(){
     
     background(220);
     drawBoard();
-    drawPlayers();
-   
-
-    
-    
-   
-}
+    drawPlayers();   
+}   
