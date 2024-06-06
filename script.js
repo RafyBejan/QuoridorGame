cf007552375a568f3ee77788e0d34c9f25c69d76
Eusebiu <eusebiu.leontescu@emanuel.ro>
2024-06-06 08:43:46 +0300

    adaugare logica ziduri

--- script.js
+++ script.js
@@ -1,34 +1,17 @@
-function setup(){
-    let canvas = createCanvas (600, 600);
-canvas.parent('game-board');
-           //canvas.center();
-// Calcularea dimensiunii unei celule pe baza lățimii canvas-ului și numărului de celule pe tabla de joc
-cellSize = width / boardSize;
-board = createBoard();
-// Inițializarea jucătorului 1 în poziția (4, 0) cu culoarea roșie
-player1= createPlayer(4,0,[255,0,0]);
-// Inițializarea jucătorului 2 în poziția (4, 8) cu culoarea albastră
-player2=createPlayer(4,8,[0,0,255]);
-//mousePressed();(e predefinita si nu e obligatoriu sa fie definita in setup )
-currentPlayer = player1;
-mousePressed = handleMousePressed;
-
-
-
-
+function setup() {
+    let canvas = createCanvas(600, 600);
+    canvas.parent('game-board');
+    cellSize = width / boardSize;
+    board = createBoard();
+    player1 = createPlayer(4, 0, [255, 0, 0]);
+    player2 = createPlayer(4, 8, [0, 0, 255]);
+    currentPlayer = player1;
+    mousePressed = handleMousePressed;
 }
+
 function draw() {
     background(220);
     drawBoard();
     drawPlayers();
-    drawWalls(); // Apelează funcția pentru a desena zidurile
-    // Alte operații de desenare sau actualizare a ecranului...
-}
-function mousePressed() {
-    handleMousePressed();
-}
-
-
-function activateWallPlacement() {
-    placingWall = true; // Setăm variabila placingWall la true pentru a activa modul de plasare a zidurilor
+    drawWalls();
 }
--- tabla.js
+++ tabla.js
@@ -1,9 +1,8 @@
-
 let boardSize = 9;
 let cellSize;
 let board;
 
-// Constructor pentru obiectul celulă
+
 function Cell(x, y, size) {
     this.x = x;
     this.y = y;
@@ -15,10 +14,10 @@ function createBoard() {
     let newBoard = [];
     for (let i = 0; i < boardSize; i++) {
         for (let j = 0; j < boardSize; j++) {
-            // Calculează coordonatele colțului stâng-sus al fiecărei celule
+           
             let x = i * cellSize;
             let y = j * cellSize;
-            // Creează un nou obiect celulă și adaugă-l în tablă
+            
             let cell = new Cell(x, y, cellSize);
             newBoard.push(cell);
         }
@@ -28,7 +27,7 @@ function createBoard() {
 function drawBoard() {
     for (let i = 0; i < board.length; i++) {
         let cell = board[i];
-        // Desenează o linie neumplută pentru a delimita fiecare celulă
+        
         stroke('black');
         noFill();
         rect(cell.x, cell.y, cell.size, cell.size);
